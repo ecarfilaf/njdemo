ffca8a8967d99b7cb74cd3e0c90326efaa7e2640
Esteban Carfilaf <ecarfilaf@gmail.com>
2018-09-18 23:40:52 -0300

    Ok

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var users = require('users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req);
   res.render('index', { title: 'Express' });
+});
+
+router.get('/users', function(req, res, next) {
+  res.render(users(req,res,next));
 });
 
 module.exports = router;
