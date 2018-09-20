a68065a45974ae58deef53ead535ec92d420742a
Esteban Carfilaf <ecarfilaf@gmail.com>
2018-09-19 23:37:26 -0300

    Ok

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var users = require('users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-router.get('/users', function(req, res, next) {
-  res.render(users(req,res,next));
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/users', function(req, res, next) {
+  console.log(req);
   res.send('respond with a resource');
 });
 
+router.get('/users/login', function(req, res, next) {
+  console.log(req);
+  res.render('/users/login');
+});
 
 module.exports = router;
